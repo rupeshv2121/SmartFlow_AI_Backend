5ddf3777b863589d65157044224e281228079a7c
Prakhar Saxena <prakharsaxena0612@gmail.com>
2026-03-27 17:57:36 +0530

    x added

--- src/routes/settings.ts
+++ src/routes/settings.ts
@@ -1,4 +1,4 @@
-ximport express, { type IRouter } from "express";
+import express, { type IRouter } from "express";
 import { settingsStore } from "../store/settings-store";
 
 const router: IRouter = express.Router();
